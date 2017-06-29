818ce073572fc4a82d84d510e73a53667c6b6e37
Manas Mishra <manasmishra@Manas-MacBook-Air.local>
2017-06-29 10:26:17 +0530

    first commit

---
+++ VisitingCardScanDemo/ScanDetailViewController.swift
@@ -0,0 +1,35 @@
+//
+//  ScanDetailViewController.swift
+//  VisitingCardScanDemo
+//
+//  Created by Manas Mishra on 29/06/17.
+//  Copyright © 2017 Manas Mishra. All rights reserved.
+//
+
+import UIKit
+
+class ScanDetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ VisitingCardScanDemo/VisitingCardTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  VisitingCardTableViewCell.swift
+//  VisitingCardScanDemo
+//
+//  Created by Manas Mishra on 29/06/17.
+//  Copyright © 2017 Manas Mishra. All rights reserved.
+//
+
+import UIKit
+
+class VisitingCardTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
